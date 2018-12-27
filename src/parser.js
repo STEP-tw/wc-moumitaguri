b096823e91cfb7b5355f70a3dd2d44b5fca56f64
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-12-27 19:39:49 +0530

    Implemented all options given together

--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,6 @@
 const HYPHEN = "-";
 
-const isOption = function(option) {
+const isOption = function (option) {
   return option.startsWith(HYPHEN);
 };
 
@@ -8,34 +8,42 @@ const WC_OPTIONS = ["-l", "-w", "-c"];
 
 const OPTIONS = { "-l": "line", "-w": "word", "-c": "byte" };
 
-const isValidOption = function(option) {
+const isValidOption = function (option) {
   return WC_OPTIONS.includes(option);
 };
 
-const createArgsObject = function(files, option) {
+const createArgsObject = function (files, option) {
   return {
     files: files,
     option: option
   };
 };
 
-const parseInput = function(args) {
+const isPossibleOption = function (option) {
+  return isOption(option) && option.length == 4;
+}
+
+const parse = function (args) {
   const firstArg = args[0];
   let files = args.slice();
   let option = "";
+  if (isPossibleOption(firstArg)) {
+    return createArgsObject(args.slice(1), option);
+  }
   if (isOption(firstArg)) {
     option = firstArg;
     files = args.slice(1);
-  }
-  return createArgsObject(files, option);
-};
-
-const parse = function(args) {
-  let { files, option } = parseInput(args);
-  if (isValidOption(option)) {
     option = OPTIONS[option];
   }
   return createArgsObject(files, option);
 };
+
+/*const parse = function(args) {
+  let { files, option } = parseInput(args);
+  if (isValidOption(option)) {
+   
+  }
+  return createArgsObject(files, option);
+};*/
 
 module.exports = { parse };
